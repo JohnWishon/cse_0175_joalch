a480bc3f556d2729ac40058826377a13d7e85076
Chris Tetreault <Chris Tetreault>
2017-01-30 16:09:40 -0800

    got globals working

--- defines.asm
+++ defines.asm
@@ -1,5 +1,23 @@
+        ;; The global struct
+
+        ;; ds.b -> byte
+        ;; ds.w -> word
+        ;; ds.p -> "3-byte pointer", whaterver that is
+        ;; ds.l -> double-word
+
+        ;; these lables are the *offsets* past global to be used. so to get a field
+        ;; test1, I do (global + test1)
+
+        defc globalAddr = global
+        defvars globalAddr
+        {
+        test1 ds.p 1
+        test2 ds.p 1
+        }
+
         ;; Functions
         defc print = $203c
+        defc openChannel = $1601
 
         ;; Constants
         defc newline = 13
