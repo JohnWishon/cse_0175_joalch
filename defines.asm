3c2211cdc4e2bbfe65a31c449607a7f8316c8c0b
Chris Tetreault <chris@doingmyprogramming.com>
2017-02-10 10:14:55 -0800

    added the updates array

--- defines.asm
+++ defines.asm
@@ -34,6 +34,8 @@ tgaClimbable:       equ %0100$0000
 tgaDrainsInterest:  equ %1000$0000
 tgaDestroyableMask: equ %0000$1111
 
+numCats:        equ 2
+numMice:        equ 4           ; TODO: Amanda
 
 
         ;; ---------------------------------------------------------------------
@@ -72,6 +74,7 @@ p2AirState: defb airStateGround
         ;; TODO: add all tile types
         ;; layout: |b0|b1|b2|b3|b4|b5|b6|b7|
         ;;         |attr|gameplay attribute|pad 0|pad 1|pad 2|pad 3|pad 4|pad 5|
+tileInstanceBase:
 tileEmpty:          defb 0, 0, 0, 0, 0, 0, 0, 0, 0, tgaPassable
                     defb 0, 0, 0, 0, 0, 0 ; Padding to 16 bytes
 tileFloor:          defb 0, 0, 0, 0, 0, 0, 0, 0, 0, tgaStandable
@@ -91,10 +94,10 @@ tileShelf:          defb 0, 0, 0, 0, 0, 0, 0, 0, 0, (tgaStandable | tgaPassable)
 
         ;; - This is a 2 dimensional array of indices into the instances area.
         ;; state[n][m] is 1 byte, where the lower 4 bits represent the damage of
-        ;; the tile at screen location <n, m>, and the upper 4 bits represent
-        ;; an address of one of the tile instances. Tile instances are padded
-        ;; to 16 bytes. This means that we can represent exactly 16 types of
-        ;; tiles in the environment!
+        ;; the tile at level location <n + 1, m + 1>, and the upper 4 bits
+        ;; represent an address of one of the tile instances. Tile instances are
+        ;; paddedto 16 bytes. This means that we can represent exactly 16 types
+        ;; of tiles in the environment!
 
         ;; Less than the actual entire screen is represented here. The bottom
         ;; row of tiles is guaranteed to be "floor" and the left and right
@@ -105,5 +108,56 @@ tileShelf:          defb 0, 0, 0, 0, 0, 0, 0, 0, 0, (tgaStandable | tgaPassable)
         ;; level. This means the array of interactable blocks needs to be
         ;; 30 * 18 tiles.
 
+        ;; - gameLevel[0][0] is the top-left corner of the interactable area.
+        ;; So one tile from the left of the screen, and 5 tiles from the top.
+        ;; - gameLevel[29][17] is the bottom-right corner of the interactable
+        ;; area. So 30 + 1 tiles from the left of the screen, and 18 + 5 tiles
+        ;; from the top.
+
 gameLevel:      defs (30 * 18)  ; should zero-fill 30 * 18 bytes
         ;; http://pasmo.speccy.org/pasmodoc.html#dirds
+
+fuNoUpdate:     equ #FF
+        ;; - This is the updates array produced at the end of an update frame.
+        ;; The data layout looks like this:
+        ;; |cat1 new|cat2 new|mouse1 new|...|mouseN new|tile1 new|tile2 new|
+        ;; |cat2 old|cat2 old|mouse1 old|...|mouseN old|tile1 old|tile2 old|
+
+        ;; - For any frame, 1 .. 2 can move, 1 .. numMice can move, and since
+        ;; each cat can attack 1 block per frame, up to 2 blocks can change in a
+        ;; frame. To use this, one could load fuNewState into IX:
+
+        ;; ld ix,fuNewState
+
+        ;; To load the index tile2 idx:
+        ;; ld a, (ix + (numCats + numMice + 1))
+
+        ;; To load the old state of tile2:
+        ;; ld a, (ix + (fuOldStateOffset + numCats + numMice + 1))
+
+        ;; For Cats:
+        ;; TODO: determine what new and old mean
+
+        ;; For Mice:
+        ;; TODO: determine what new and old mean
+
+        ;; For Tiles:
+        ;; - tileN new: This is an index into the gameLevel array. This index
+        ;; tells you what <x,y> coordinate the block is at subject to the
+        ;; constraints specified in the docuemnation for the gameLevel array.
+        ;; The tile at that location can be found in the gameLevel array
+        ;; - tileN old: This is an index into tileInstanceBase to get the
+        ;; old tile if needed.
+
+
+fuNewState: defs numCats
+            defs numMice
+            defs (numCats * 2) ; These need to be words because 30 * 18 > 255
+                               ; and this is an index into gameLevel
+fuOldState: defs numCats
+            defs numMice
+            defs (numCats) ; These do not need to be words because there are
+                           ; only 16 kinds of tiles, and this is an index into
+                           ; tileInstanceBase
+
+fuOldStateOffset:       equ fuOldState - fuNewState
