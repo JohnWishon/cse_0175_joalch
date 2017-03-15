69023aa06d45ed627316cab59838a3e8c34bedfa
Chris Tetreault <chris@doingmyprogramming.com>
2017-03-15 10:38:20 -0700

    vertical collisions work-ish
    off-by-one with vertical collide up

--- defines.asm
+++ defines.asm
@@ -272,8 +272,8 @@ catPoseFaceLeftClearMask: equ %0111$1111
 fuP1UpdatesBase:
 fuP1UpdatesOldPosX:       defb 8 * 10 + 0
 fuP1UpdatesNewPosX:       defb 8 * 10 + 0
-fuP1UpdatesOldPosY:       defb 8 * 9 + 0
-fuP1UpdatesNewPosY:       defb 8 * 9 + 0
+fuP1UpdatesOldPosY:       defb 8 * 13 + 6
+fuP1UpdatesNewPosY:       defb 8 * 13 + 6
 fuP1UpdatesOldPose:       defb 0
 fuP1UpdatesNewPose:       defb catPoseWalk | catPoseFaceLeft
 fuP1UpdatesTileChangeX:   defb 10
