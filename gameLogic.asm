6a76498ce02fd0ca4ac87c24df261e038453cef2
JohnWishon <jwishon@ucsd.edu>
2017-03-16 08:51:32 -0700

    gL add

--- gameLogic.asm
+++ gameLogic.asm
@@ -9,12 +9,53 @@ setupGameLogicYLoop:
         ld c, levelTileWidth
 setupGameLogicXLoop:
         dec c
-        ;; ld (hl), (dynamicTileTestImpassableOneHealth - dynamicTileInstanceBase) | 1
+        ;ld (hl), (dynamicTileTestImpassableOneHealth - dynamicTileInstanceBase) | 1
+
+
         inc hl
         ld a, c
         cp 0
         jp nz, setupGameLogicXLoop
         djnz setupGameLogicYLoop
+
+        ;; Fish tank is impassable
+        ld hl, gameLevel
+        ld bc, 8 + (9 * levelTileWidth)
+        add hl, bc
+        ld (hl), tgaNone
+
+        inc hl
+        ld (hl), tgaNone
+
+        ld hl, gameLevel
+        ld bc, 8 + (10 * levelTileWidth)
+        add hl, bc
+        ld (hl), tgaNone
+
+        inc hl
+        ld (hl), tgaNone
+
+        ;;  Fish tank shelf is passable/standable
+        ld hl, gameLevel
+        ld bc, 7 + (11 * levelTileWidth)
+        add hl, bc
+        ld (hl), tgaPassable | tgaStandable
+
+        inc hl
+        ld (hl), tgaPassable | tgaStandable
+
+        inc hl
+        ld (hl), tgaPassable | tgaStandable
+
+        inc hl
+        ld (hl), tgaPassable | tgaStandable
+
+        inc hl
+        ld (hl), tgaPassable | tgaStandable
+
+        inc hl
+        ld (hl), tgaPassable | tgaStandable
+
         ret
 
 updateGameLogic:
