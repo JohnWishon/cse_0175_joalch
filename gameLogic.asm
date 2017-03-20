65bf10766c501df0d7ce26a5bca12b75cb5c5e83
Huajie Wu <wuhuajie.flyfire2002@gmail.com>
2017-03-20 15:59:23 -0700

    Added end game screen

--- gameLogic.asm
+++ gameLogic.asm
@@ -219,7 +219,7 @@ updateGameLogic:
         ;;*      signal mouse kill
 
         ld  a,(interestDrainCounter)    ; Get the interest drain counter
-        cp  25 * 4                      ; 25 * sec, 8 secs for now
+        cp  25 * 2                      ; 25 * sec, 8 secs for now
         jp  c,logicNoDrainYet           ; If the counter is lower than the bar, no drain
         ld  a, 0
         ld  (interestDrainCounter),a    ; Reset the counter
